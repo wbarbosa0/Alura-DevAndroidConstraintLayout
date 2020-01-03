4226d801a4d9090e6da1497f292b228c4b62db2a
wbarbosa0 <wbarbosa.br@gmail.com>
2020-01-03 15:35:01 -0300

    Posição após a lição 2 da parte 1 "Criando o banner"

---
+++ app/src/main/java/br/nom/wbarbosa/wbviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package br.nom.wbarbosa.wbviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.nom.wbarbosa.wbviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
